a13779aef57625161dbd3adc665f49b524ee2356
Amir Khan <amirkhan1985@gmail.com>
2023-11-05 23:46:45 +0530

    update

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import Card from './Card'
+export default function Home() {
+    return (
+        <>
+            <div className="container">
+             <Card/>
+             <Card/>
+             <Card/>
+
+
+            </div>
+        </>
+    )
+}
